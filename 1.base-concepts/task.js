328fd0ac1f4a1b0a78102a8de2140668fb5093a7
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2021-12-08 20:51:31 +0300

    Первый вариант решения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr = [];
+    let diskrim = b ** 2 - 4 * a * c; //Вычисление дискриминанта
+
+    if (diskrim < 0) {
+        arr[0];
+        arr[1];
+    } else if (diskrim > 0) {
+        let x = -b / (2 * a);
+        arr[0] = x;
+        arr[1];
+    } else {
+        let x1 = (-b + Math.sqrt(diskrim)) / (2 * a);
+        let x2 = (-b - Math.sqrt(diskrim)) / (2 * a);
+        arr[0] = x1;
+        arr[1] = x2;
+    }
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let totalAmount;
 
-  // код для задачи №2 писать здесь
+    // код для задачи №2 писать здесь
 
-  return totalAmount;
+    return totalAmount;
 }
