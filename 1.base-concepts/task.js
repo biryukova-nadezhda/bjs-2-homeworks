3b27a653836bdb10778649156f4654ea20cb9d78
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2021-12-10 18:21:31 +0300

    Исправленный вариант первой задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,19 +2,17 @@ function solveEquation(a, b, c) {
     let arr = [];
     let diskrim = b ** 2 - 4 * a * c; //Вычисление дискриминанта
 
-    if (diskrim < 0) {
-        arr[0];
-        arr[1];
-    } else if (diskrim > 0) {
-        let x = -b / (2 * a);
-        arr[0] = x;
-        arr[1];
-    } else {
+    if (diskrim > 0) {
         let x1 = (-b + Math.sqrt(diskrim)) / (2 * a);
         let x2 = (-b - Math.sqrt(diskrim)) / (2 * a);
         arr[0] = x1;
         arr[1] = x2;
+    } else if (diskrim === 0) {
+        let x = -b / (2 * a);
+        arr[0] = x;
+        arr[1];
     }
+
     return arr; // array
 }
 
