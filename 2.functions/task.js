ebbeddbf5966c797a2df518127ba2825421a65c7
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2021-12-12 23:09:12 +0300

    Решение первой задачи

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    let avg = 0;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
 
-  return { min: min, max: max, avg: avg };
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+        sum += arr[i];
+        avg = parseFloat((sum / arr.length).toFixed(2));
+    }
+
+    return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum;
 
-  // Ваш код
+    // Ваш код
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
 }
