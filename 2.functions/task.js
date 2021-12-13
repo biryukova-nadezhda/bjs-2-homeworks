2e16dbc09ed77865ab3eef78f96dcb652a2cde14
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2021-12-13 19:13:03 +0300

    Решение трех задач на проверку. Тест пройден

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -23,23 +23,46 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-    let sum;
+    let sum = 0;
 
-    // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
 
     return sum;
 }
 
 function makeWork(arrOfArr, func) {
-    let max;
+    let max = -Infinity;
+    let sum = 0;
 
-    // Ваш кода
-    // for ...
+    for (let i = 0; i < arrOfArr.length; i++) {
+        sum = func(arrOfArr[i]);
 
+        if (sum > max) {
+            max = sum;
+        }
+    }
     return max;
 }
 
 // Задание 3
 function worker2(arr) {
-    // Ваш код
+    let min = Infinity;
+    let max = -Infinity;
+    let diff = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+
+        diff = Math.abs(max - min);
+    }
+
+    return diff;
 }
