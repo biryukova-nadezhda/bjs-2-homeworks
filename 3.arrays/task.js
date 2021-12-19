1c7d04b07932b02877267e943d1e0aa34d7df2b2
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2021-12-19 11:06:27 +0300

    Решение двух задач на проверку

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
+    let result;
 
-  // Ваш код
+    result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
-  return result; // boolean
+    return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr;
 
-  // Ваш код
+    resultArr = arr.filter((item) => item >= 0 && item % 3 === 0).map((item) => item * 10);
 
-  return resultArr; // array
+    return resultArr; // array
 }
