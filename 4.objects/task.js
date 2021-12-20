e49592884b7aaf444b08a4adccad851ddaa91bad
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2021-12-20 18:07:05 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+let students1 = new Student("Надежда", "Женщина", 25);
+let students2 = new Student("Владислав", "Мужчина", 24);
+let students3 = new Student("Валерия", "Женщина", 23);
+let students4 = new Student("Сергей", "Мужчина", 19);
+let students5 = new Student("Николай", "Мужчина", 50);
+let students6 = new Student("Самира", "Женщина", 42);
+
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+
+    if (this.marks === undefined) {
+        this.marks = [];
+        this.marks.push(mark);
+
+    } else {
+        this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...arg) {
+    this.marks = [];
+    this.marks.push(...arg);
+}
+
+Student.prototype.getAverage = function() {
+    let sum = 0;
+    this.marks.map(item => sum += item);
+    return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
+}
